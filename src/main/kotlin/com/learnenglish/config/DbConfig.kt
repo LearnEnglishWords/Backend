7e637b606651265a2e8467a2c9a2b5b25e147c2c
Martin Jablecnik <martin.jablecnik@email.cz>
2020-03-18 12:45:53 +0100

    Add environment variables

--- src/main/kotlin/com/learnenglish/config/DbConfig.kt
+++ src/main/kotlin/com/learnenglish/config/DbConfig.kt
@@ -2,8 +2,11 @@ package com.learnenglish.config
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import io.micronaut.context.ApplicationContext
 import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Replaces
+import io.micronaut.context.annotation.Value
+import io.micronaut.context.env.Environment
 import org.jdbi.v3.core.Jdbi
 import javax.inject.Singleton
 import javax.sql.DataSource
@@ -21,10 +24,14 @@ class DbConfig {
     @Factory
     @Replaces(DataSource::class)
     fun dataSource(): DataSource {
+        val mysqlAddress = System.getenv("MYSQL_ADDRESS")
+        val mysqlPort = System.getenv("MYSQL_PORT")
+        val mysqlDatabase = System.getenv("MYSQL_DATABASE")
+
         var config = HikariConfig()
-        config.jdbcUrl = "jdbc:mysql://drakeman.cz:3306/learnenglish"
-        config.username = "root"
-        config.password = "test1"
+        config.jdbcUrl = "jdbc:mysql://${mysqlAddress}:${mysqlPort}/${mysqlDatabase}"
+        config.username = System.getenv("MYSQL_USER")
+        config.password = System.getenv("MYSQL_PASSWORD")
         return HikariDataSource(config)
     }
 }
